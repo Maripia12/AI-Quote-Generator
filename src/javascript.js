bdab566f805ed02a83c1c3f2754d1a9c72346bea
Pia Paulino <maripia1220@gmail.com>
2024-11-18 16:28:03 -0500

    Improve user experience

--- src/javascript.js
+++ src/javascript.js
@@ -15,11 +15,15 @@ function displayQuote(response){
 function generateQuote(event){
     event.preventDefault()
     console.log('clicked')
-    // let quoteDisplayElement = document.querySelector('#quote-display')
-    let userPrompInputtElement = document.querySelector('#user-prompt')
+   
+    let userInputtElement = document.querySelector('#user-prompt')
+    let quoteDisplayElement = document.querySelector('#quote-display')
+    quoteDisplayElement.classList.remove("hidden")
+    quoteDisplayElement.innerHTML = `<div class="generating"> ⏳ Generating a quote about ${userInputtElement.value}</div>`
+
     let apiKey = 'a03e62525c5f6b05f4bt9c6f57oe33ff'
     let context = "You are an amazing quote generator. Please display quote in one html line and separate each line with a <br />. Sign the quote inside a <strong> element"
-    let prompt = `Generate a quote about ${userPrompInputtElement.value}`
+    let prompt = `Generate a quote about ${userInputtElement.value}`
     let apiUrl= `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`
 
     axios(apiUrl).then(displayQuote)
