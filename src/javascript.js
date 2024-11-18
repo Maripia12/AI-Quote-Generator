fb957d82cc873189a9c1b1a352a3c127f0ab03cb
Pia Paulino <maripia1220@gmail.com>
2024-11-18 12:31:29 -0500

    added basic form handling and typewriter plugin

--- src/javascript.js
+++ src/javascript.js
@@ -1,1 +1,21 @@
-// alert('hi')
+function generateQuote(event){
+    event.preventDefault()
+    console.log('clicked')
+    // let quoteDisplayElement = document.querySelector('#quote-display')
+    // let apiKey = ''
+    // let apiUrl= ``
+
+    new Typewriter('#quote-display', {
+        strings: 'Hello Pia, how are you today?',
+        autoStart: true,
+        cursor: null,
+        delay:1
+      });
+    
+
+   
+}
+
+
+let formElement = document.querySelector("#quote-generator-form")
+formElement.addEventListener("submit", generateQuote)
